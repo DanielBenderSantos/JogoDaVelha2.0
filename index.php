a02cf3d949bd4a40a781536b3cb1704dfb6b4f3a
Daniel Bender <47634362+DanielBenderSantos@users.noreply.github.com>
2023-03-22 11:57:22 -0300

    Add files via upload

---
+++ index.php
@@ -0,0 +1,33 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Tela incial</title>
+    <link rel="stylesheet" type="text/css" media="screen" href="assets/css/index.css">
+    <script type="text/javascript"  src="assets/js/index.js"></script>
+</head>
+<body>
+    <div class='todo'>
+        <div class='JogoCentral'>
+            <div class='linha'>
+                <button type="button" class="botao" onclick="jogada(1)" value="0" id="1"></button>  
+                <button type="button" class="botao" onclick="jogada(2)" value="0" id="2"></button> 
+                <button type="button" class="botao" onclick="jogada(3)" value="0" id="3"></button>
+            </div>
+            <div class='linha'>
+                <button type="button" class="botao" onclick="jogada(4)" value="0" id="4"></button>  
+                <button type="button" class="botao" onclick="jogada(5)" value="0" id="5"></button> 
+                <button type="button" class="botao" onclick="jogada(6)" value="0" id="6"></button>
+            </div>
+            <div class='linha'>
+                <button type="button" class="botao" onclick="jogada(7)" value="0" id="7"></button>  
+                <button type="button" class="botao" onclick="jogada(8)" value="0" id="8"></button> 
+                <button type="button" class="botao" onclick="jogada(9)" value="0" id="9"></button>
+            </div> 
+        </div>
+    
+    </div>
+</body>
+</html>
